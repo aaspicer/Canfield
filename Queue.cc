7aab31054ff61ca15350ab99386502741d071bb7
aaspicer <55764431+aaspicer@users.noreply.github.com>
2019-09-24 19:31:38 -0400

    add the files

---
+++ Queue.cc
@@ -0,0 +1,71 @@
+/*********************************************************************
+  Author:        Dana Vrajitoru, IUSB, CS
+  Class:         C243  Data Structures
+  File name:     Queue.cc
+  Last updated:  September 2015
+  Description:   Implementation of a class queue derived from a list.
+**********************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#include "Queue.h"
+
+// Default constructor: create an empty stack.
+Queue::Queue() : List()
+{
+    // Code to be supplied by the student.
+} // Queue::Queue();
+
+// Insert an object into the queue.
+void Queue::enqueue(int data)
+{
+    insertBack(data);
+} // Queue::enqueue
+
+// Remove the front object and return its value.
+int Queue::dequeue()
+{
+    int result = front();
+    removeFront();
+    return result;
+} //Queue::dequeue()
+
+// Inspect the value of the front object.
+int Queue::front()
+{
+    if(!isEmpty())
+        return *begin();
+    else
+    {
+        cout << "attempt to access the top of an empty stack" << endl;
+        exit(1);
+    }
+    // Code to be supplied by the student.
+} // Queue::front()
+
+// Delete all the objects in the queue.
+void Queue::makeEmpty()
+{
+    List::makeEmpty();
+} // Queue::makeEmpty()
+
+// Test if the queue is empty.
+bool Queue::isEmpty()
+{
+    return List::isEmpty();
+} // Queue::isEmpty()
+
+// Concatenate two queues.
+void Queue::concatenate(Queue &other)
+{
+    List::concatenate(other);
+} // Queue::concatenate()
+
+// The number of objects in the queue
+int Queue::stored()
+{
+    return getSize();
+} // Queue::stored()
+
